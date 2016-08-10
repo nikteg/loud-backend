18bae0348e2888fbc63b25897d3016c55aa9429d
Niklas <me@sodapop.se>
2016-08-11 00:11:13 +0200

    Remove plugs not necessary for an api

--- lib/loud_backend/endpoint.ex
+++ lib/loud_backend/endpoint.ex
@@ -23,16 +23,16 @@ defmodule LoudBackend.Endpoint do
     pass: ["*/*"],
     json_decoder: Poison
 
-  plug Plug.MethodOverride
-  plug Plug.Head
+  # plug Plug.MethodOverride
+  # plug Plug.Head
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_loud_backend_key",
-    signing_salt: "8bJxqp7M"
+  # plug Plug.Session,
+  #   store: :cookie,
+  #   key: "_loud_backend_key",
+  #   signing_salt: "8bJxqp7M"
 
   plug CORSPlug
 
