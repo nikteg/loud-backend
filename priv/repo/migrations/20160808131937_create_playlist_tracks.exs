05bdaf5d78df70efcef2ef54606af797bd030ed4
Niklas <me@sodapop.se>
2016-08-14 21:49:31 +0200

    Delete all associated tracks on playlist removal

--- priv/repo/migrations/20160808131937_create_playlist_tracks.exs
+++ priv/repo/migrations/20160808131937_create_playlist_tracks.exs
@@ -3,7 +3,7 @@ defmodule LoudBackend.Repo.Migrations.CreatePlaylistTracks do
 
   def change do
     create table(:playlist_tracks, primary_key: false) do
-      add :playlist_id, references(:playlists)
+      add :playlist_id, references(:playlists, on_delete: :delete_all)
       add :track_id, references(:tracks)
     end
   end
